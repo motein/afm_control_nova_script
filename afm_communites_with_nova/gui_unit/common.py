b744669f6da12c54375d2ccb6c37a3c096858ba7
motein <motein@126.com>
2018-06-28 14:51:42 +0800

    Select Path or File and save it in a variable.

--- afm_communites_with_nova/gui_unit/common.py
+++ afm_communites_with_nova/gui_unit/common.py
@@ -6,10 +6,20 @@ Created on Jun 28, 2018
 '''
 from PySide import QtGui
 
-def select_directory():
-    selected_directory = QtGui.QFileDialog.getExistingDirectory()
-    return selected_directory
+def select_directory(selected_directory):
+    selected_directory.value = QtGui.QFileDialog.getExistingDirectory()
 
-def select_file():
-    selected_file = QtGui.QFileDialog.getOpenFileName()
-    return selected_file
+def select_file(selected_file):
+    selected_file.value = QtGui.QFileDialog.getOpenFileName()
+
+def show_message(message_wrapper):
+    msgBox = QtGui.QMessageBox()
+    message = message_wrapper.value
+    if (isinstance(message, tuple)):
+        message = message[0]
+    msgBox.setText("Message: " + message)
+    msgBox.exec_()
+
+class PathWrapper:
+    def __init__(self, path = None):
+        self.value = path
--- afm_communites_with_nova/gui_unit/test_common.py
+++ afm_communites_with_nova/gui_unit/test_common.py
@@ -3,14 +3,48 @@ Created on Jun 28, 2018
 
 @author: xiongan2
 '''
+import sys
 from PySide import QtCore, QtGui
-from gui_unit.common import select_directory, select_file
+from functools import partial
+from gui_unit.common import select_directory, select_file, show_message, PathWrapper
 
-x = None
-app = QtGui.QApplication([])
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(564, 435)
+        self.path = PathWrapper("")
+        self.select_button = QtGui.QPushButton(Form)
+        self.select_button.setGeometry(QtCore.QRect(250, 170, 211, 20))
+        self.select_button.setObjectName("Select File")
+        self.select_button.clicked.connect(partial(select_directory, self.path))
+        
+        self.show_button = QtGui.QPushButton(Form)
+        self.show_button.setGeometry(QtCore.QRect(380, 270, 75, 23))
+        self.show_button.setObjectName("Show Button")
+        self.show_button.clicked.connect(partial(show_message, self.path))
 
-button = QtGui.QPushButton("Select File")
-button.clicked.connect(select_file)
-button.show()
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-app.exec_()
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.select_button.setText(QtGui.QApplication.translate("Form", "PushButton", None, QtGui.QApplication.UnicodeUTF8))
+        self.show_button.setText(QtGui.QApplication.translate("Form", "PushButton", None, QtGui.QApplication.UnicodeUTF8))
+
+def test():
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+    
+
+def test2():
+    path = PathWrapper()
+    select_directory(path)
+    print(path.value)
+    
+
+if __name__ == '__main__':
+    test()
